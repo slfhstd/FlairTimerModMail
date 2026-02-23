a8b527abafea212c77d085b26b35e6791fb95413
Slfhstd <slfhstd@gmail.com>
2026-02-23 14:00:38 +0000

    32-37

--- flairtimermodmail.py
+++ flairtimermodmail.py
@@ -29,7 +29,12 @@ def main(reddit, posts: dict):
                 if time.time() > posts[submission] + (config.hours * 60 * 60):
                     posts.pop(submission)
                     reddit.submission(submission).save()
-                    reddit.subreddit(config.subreddit).modmail.create(subject=f"{config.messagetitle}", body=f"It has been {config.hours/24} day/s since this was flaired [{config.flair_text}](https://old.reddit.com{reddit.submission(submission).permalink})", recipient=config.modmail_recipient)
+                    data = {
+                        "subject": config.messagetitle,
+                        "text": f"It has been {config.hours/24} day/s since this was flaired [{config.flair_text}](https://old.reddit.com{reddit.submission(submission).permalink})",
+                        "to": "/r/{}".format(config.modmail_recipient),
+                    }
+                    reddit.post("api/compose/", data=data)
                     print(f"Post {submission} has been flaired {config.flair_text} for {config.hours * 60} minutes, sent modmail")
                     break
  
